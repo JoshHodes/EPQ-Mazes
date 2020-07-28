efb5c8fee2c4db053031cc87264f060bfc08fae8
Josh Hodes <joshhodes2004@gmail.com>
2020-07-28 21:03:54 +0100

    Added notes of plans

--- Dijkstra.js
+++ Dijkstra.js
@@ -6,6 +6,7 @@ const maze = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 2, 0],
 ];
+
 for (i = 0; i <= maze.length - 1; i++) {
     let row = "";
     for (j = 0; j <= maze[i].length - 1; j++) {
@@ -14,3 +15,8 @@ for (i = 0; i <= maze.length - 1; i++) {
     }
     console.log(row)
 }
+// need to be able to find all the legal adjacent spaces, and put them all in a queue
+// keep going until the 2 points in the maze meet
+// a coord is legal if: it does not exceed the boundaries of the maze, and if it is not a source coord, and if it is not a wall,
+// and if it has not been visited before
+// adjacent meaning up, down, left and right
